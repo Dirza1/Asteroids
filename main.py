9044e2c410a9f6d1a3e76ca0051d98e06399203c
Dirza1 <jasper.olthof@xs4all.nl>
2025-02-19 11:19:10 +0100

    Optimised recouse useage

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    time_object = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = time_object.tick(60)/1000
+
 
 
 
