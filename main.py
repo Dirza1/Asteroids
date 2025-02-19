3eebbb19f2ada464e64e6e6981b4e1ea59a88bb9
Dirza1 <jasper.olthof@xs4all.nl>
2025-02-19 11:32:28 +0100

    break time

--- main.py
+++ main.py
@@ -3,19 +3,23 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     time_object = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = time_object.tick(60)/1000
+        
 
 
 
